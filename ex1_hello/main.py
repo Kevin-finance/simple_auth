6f8ed5d02b7bdd7d33680bb6386d6ef31284d0ad
Kevin-finance <mooseok1007@gmail.com>
2025-03-06 20:29:14 -0600

    vercel.json change

--- ex1_hello/main.py
+++ ex1_hello/main.py
@@ -4,10 +4,10 @@ from fastapi.staticfiles import StaticFiles
 app = FastAPI()
 
 # Mount the "main" directory to serve static files
-app.mount("/", StaticFiles(directory="main", html=True), name="main")
+app.mount("/", StaticFiles(directory="ex1_hello/main", html=True), name="main") # 안되는데 ? 
 
 # Don't use this for Vercel: This is just for demo purposes
 # If you use the code below, then run it with `python main.py`
-# if __name__ == "__main__":
-#     import uvicorn
-#     uvicorn.run(app, host="0.0.0.0", port=8000)
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
